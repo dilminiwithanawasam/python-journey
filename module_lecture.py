fbb633893aba46226ac166d3b0398b421fcd4e91
Dilmini Withanawasam <dilminiwithanawasam1250@gmail.com>
2025-10-31 22:54:33 +0530

    learnt modules

---
+++ module_lecture.py
@@ -0,0 +1,7 @@
+from my_module import calculator as c
+c(20, 5)
+# Output:
+# Addition: 25
+# Subtraction: 15
+# Multiplication: 100
+# Division: 4.0
---
+++ my_module.py
@@ -0,0 +1,10 @@
+a=10
+def area_of_circle(radius):
+    pi = 3.14159
+    return pi * radius * radius
+def calculator(x, y):
+    print("Addition:", x + y)
+    print("Subtraction:", x - y)    
+    print("Multiplication:", x * y)
+    print("Division:", x / y)
+
